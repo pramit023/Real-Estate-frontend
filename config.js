b53297b0a60b59bee95ea761cad1151e4b83b433
pramit023 <pramitpriyanshu16@gmail.com>
2026-07-03 13:42:20 +0530

    modify config.js file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://real-estate-backed.vercel.app';
+const API_URL = 'https://real-estate-backed.vercel.app/';
 // https://real-estate-backed.vercel.app/
 
 export default API_URL;
